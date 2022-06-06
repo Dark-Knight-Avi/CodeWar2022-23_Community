30ddb3cdafcd5e26498aef83366a76254ebf65d3
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-06-06 19:19:44 +0530

    Sort 0 1 2

---
+++ Aritra/CN/CN_ARRAY_0606.py
@@ -0,0 +1,44 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**7)
+
+def sort012(arr, n) :
+
+	# write your code here
+    # don't return anything 
+    arr.sort()
+
+
+#taking input using fast I/O
+def takeInput() :
+	n = int(input().strip())
+
+	if n == 0 :
+		return list(), 0
+
+	arr = list(map(int, stdin.readline().strip().split(" ")))
+
+	return arr, n
+
+
+
+def printAnswer(arr, n) :
+    
+    for i in range(n) :
+        
+        print(arr[i],end=" ")
+        
+    print()
+    
+#main
+
+t = int(input().strip())
+for i in range(t) :
+
+    arr, n= takeInput()
+    sort012(arr, n)
+    printAnswer(arr, n)
