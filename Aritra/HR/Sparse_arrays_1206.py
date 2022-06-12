6fdea6c4c3d40f717ef0d1ee101c33a2ec5deb97
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-06-12 19:50:33 +0530

    Sparse Arrays
    https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=true

---
+++ Aritra/HR/Sparse_arrays_1206.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    arr = []
+    for i in queries:
+        count = 0
+        for j in strings:
+            if i == j:
+                count += 1
+        arr.append(count)
+    return arr
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
