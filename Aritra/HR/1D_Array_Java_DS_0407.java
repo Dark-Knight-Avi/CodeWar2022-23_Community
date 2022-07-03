c9d0efc570712527239bf079432eb49e8d8f5385
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-07-04 00:43:47 +0530

    Create 1D_Array_Java_DS_0407.java

---
+++ Aritra/HR/1D_Array_Java_DS_0407.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> a = new ArrayList<Integer>();
+        for(int i = 0; i < n; i++){
+            a.add(sc.nextInt());
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(a.get(i));
+        }
+    }
+}
