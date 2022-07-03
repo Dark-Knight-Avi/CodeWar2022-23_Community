5b13cf0f6ce42a205179767e2ad231fc4ec5c572
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-07-04 00:56:51 +0530

    Java 2D Array
    https://www.hackerrank.com/challenges/java-2d-array/problem?isFullScreen=true

---
+++ Aritra/HR/2D_Array_Java_DS_0407.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a[][] = new int[6][6];
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 6; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                int sum =
+                    a[i][j]
+                    + a[i][j+1]
+                    + a[i][j+2]
+                    + a[i+1][j+1]
+                    + a[i+2][j]
+                    + a[i+2][j+1]
+                    + a[i+2][j+2];
+                if (sum > maxSum) {maxSum = sum;}
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
